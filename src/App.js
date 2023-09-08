533ded80258015844cceb1e4d1338fed0d16b654
TeresaLiang123 <teresaliangmain@gmail.com>
2023-09-07 23:04:36 -0400

    Filled id card

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
       </div>
       <div className="card">
         <div className="description">
+          <span className="identity">Identification</span>
           <Description></Description>
+          <Social></Social>
         </div>
         <Picture></Picture>
       </div>
@@ -110,6 +112,20 @@ const Picture = () => {
   return(
     <img className="pic" src="./t.jpg"></img>
   );
+}
+
+const Social = () => {
+  return(<section className="icons">
+    <a href="https://github.com/TeresaLiang123/Projects" target="https://github.com/TeresaLiang123/Projects">
+      <img src="./github-icon.png" width="50" height="50"></img>
+    </a>
+    <a href="https://www.linkedin.com/in/liang-teresa/" target="https://www.linkedin.com/in/liang-teresa/">
+      <img src="./linkedin-icon.png" width="50" height="50"></img>
+    </a>
+    <a href="" target="">
+      <img src="./gmail-icon.png" width="13%"></img>
+    </a>
+  </section>);
 }
 
 export default App;
